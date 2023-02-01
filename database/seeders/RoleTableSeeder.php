c94f4bd2c957795730a3fa2074d22a39a202e7e3
milano09 <milanojazzyadhi1@gmail.com>
2023-02-01 20:18:07 +0700

    feat: Setup Saptie

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-// use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Role;
 
 class RoleTableSeeder extends Seeder
 {
@@ -16,7 +16,7 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        // Role::create(['name' => 'Admin']);
-        // Role::create(['name' => 'User']);
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'User']);
     }
 }
